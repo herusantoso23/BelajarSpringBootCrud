63ea0e50adab57c890972ee048265a6b57fd862a
HeruSantoso <herusantoso008@gmail.com>
2017-10-29 22:22:02 +0700

    initial

---
+++ src/main/java/com/herusantoso/springboot/BelajarSpringBootCrud/BelajarSpringBootCrudApplication.java
@@ -0,0 +1,12 @@
+package com.herusantoso.springboot.BelajarSpringBootCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringBootCrudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/herusantoso/springboot/BelajarSpringBootCrud/BelajarSpringBootCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herusantoso.springboot.BelajarSpringBootCrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelajarSpringBootCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
